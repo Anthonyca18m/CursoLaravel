9d0bf579e4a6fdf32414532c6c3f9e991ccfa031
anthonyca18m@ <anthonyca18m@gmail.com>
2020-11-13 23:25:18 -0500

    Seeder de Usuarios (Profile, Location, Image & Groups)

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -20,5 +20,29 @@ class DatabaseSeeder extends Seeder
         factory(Level::class)->create(['name' => 'Oro']);
         factory(Level::class)->create(['name' => 'Placa']);
         factory(Level::class)->create(['name' => 'Bronce']);
+
+        factory(User::class, 5)->create()->each(function ($user) {
+
+            $profile = $user->profile()->save( factory(Profile::class)->make() );
+
+            $profile->location()->save( factory(Location::class)->make() );
+
+            $user->groups()->attach( $this->array(rand(1, 3)) );
+
+            $user->profile->save( factory(Image::class)->make(
+                ['url' => 'https://lorempixel.com/90/90/']
+            ) );
+        });
+    }
+
+    public function array($max)
+    {
+        $values = [];
+
+        for ($i=0; $i < $max; $i++) {
+            $values[] = $i;
+        }
+
+        return $values;
     }
 }
